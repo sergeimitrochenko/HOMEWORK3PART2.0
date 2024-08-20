ddf4bb369c5e4138dea8a5a8b376c90ab1eed558
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-08-20 21:31:22 +0300

    "Задача № 2"

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,18 @@ public class Main {
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2013;
+        int OS = 0;
+        if (OS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (OS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (OS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
     }
 }
